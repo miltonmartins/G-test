58bf23281b54102817d8f876da7f94aae5401181
Milton Martins <milton.martins@mobile2you.com.br>
2021-03-21 16:38:08 -0300

    Forced dark theme disabled

--- app/src/main/java/com/martins/milton/goktest/GokTestApplication.kt
+++ app/src/main/java/com/martins/milton/goktest/GokTestApplication.kt
@@ -1,5 +1,7 @@
 package com.martins.milton.goktest
 
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import com.martins.milton.goktest.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -8,5 +10,11 @@ open class GoKTestApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
 
         return DaggerApplicationComponent.factory().create(applicationContext)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
     }
 }
